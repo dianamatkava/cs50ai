4b684292f6233068fc6a542e8806dcf8dab283eb
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-04-20 18:26:21 +0200

    git init

---
+++ tictactoe.py
@@ -0,0 +1,74 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    # in the initial game state, X
+    # Any return value is acceptable if a terminal board is provided as input (when game is over).
+    return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    raise NotImplementedError
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    board[action[0]][action[1]] = X
+
+    # raise exception if not possible
+    # do not modify origin board
+    return board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return False
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    # If the board is a terminal board, the minimax function should return Non
+    raise NotImplementedError
