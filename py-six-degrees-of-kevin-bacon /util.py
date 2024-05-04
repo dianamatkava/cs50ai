01439d1c48b30bdd7a383cb48cdb92427e1e1b3a
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-05-04 16:10:29 +0200

    submit tictactoe

---
+++ py-six-degrees-of-kevin-bacon /util.py
@@ -0,0 +1,41 @@
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state  # movie_id start_id
+        self.parent = parent  # node
+        self.action = action  # next star
+
+    def __repr__(self):
+        return f"{self.state} {self.parent} {self.action}"
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
