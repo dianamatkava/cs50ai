48b8c5567b7bd43d642fd8095cb4d970815f486b
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-07-13 19:13:25 +0200

    Sample RankPage

--- Google PageRank/pagerank.py
+++ Google PageRank/pagerank.py
@@ -8,10 +8,10 @@ SAMPLES = 10000
 
 
 def main():
-    # if len(sys.argv) != 2:
-    #     sys.exit("Usage: python Google PageRank.py corpus")
-    # corpus = crawl(sys.argv[1])
-    corpus = crawl('corpus0')
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python Google PageRank.py corpus")
+    corpus = crawl(sys.argv[1])
+    # corpus = crawl('corpus0')
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
@@ -49,7 +49,7 @@ def crawl(directory):
     return pages
 
 
-def transition_model(corpus, page, damping_factor):
+def transition_model(corpus, page, damping_factor) -> dict:
     """
     Return a probability distribution over which page to visit next,
     given a current page.
@@ -58,7 +58,17 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    res = {key: (1-damping_factor)/len(corpus.keys()) for key in corpus.keys()}
+    for link in corpus[page]:
+        res[link] += damping_factor / len(corpus[page])
+
+    # Normalize the values to ensure their sum is exactly 1
+    total = sum(res.values())
+    for key in res:
+        res[key] /= total
+
+    assert int(sum(list(res.values()))) == 1
+    return res
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -70,7 +80,19 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    pageranks = {key: 0 for key in corpus}
+    sample_key = random.choices(list(corpus.keys()))[0]
+
+    pageranks[sample_key] = 1
+    sample = transition_model(corpus, sample_key, damping_factor)
+
+    for i in range(n-1):
+        sample_key = random.choices(list(sample.keys()), list(sample.values()), k=1)[0]
+        sample = transition_model(corpus, sample_key, damping_factor)
+        pageranks[sample_key] += 1
+
+    return {key: value / n for key, value in pageranks.items()}
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -82,7 +104,14 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    pageranks = {key: (1 - damping_factor) / len(corpus) for key in corpus}
+
+    for page in pageranks.keys():
+
+
+def rank_page():
+    pass
 
 
 if __name__ == "__main__":
